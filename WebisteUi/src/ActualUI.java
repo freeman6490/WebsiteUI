bfeb1c3a73f85d6993d52df783eff70d561d6d5c
freeman6490 <lfreeman6490@gmail.com>
2019-12-08 21:35:02 -0600

    Main class. Added some buttons. First commit. Worked on through week w/o commits. Also grabs HTML now

--- WebisteUi/src/ActualUI.java
+++ WebisteUi/src/ActualUI.java
@@ -1,12 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 /**
  * @author Luke Freeman
  * Creates the UI itself and adds buttons and other things as well
  * JButton has to come first, etc. 
  * The frame of the UI.
  */
+class ButtonHandler implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
 public class ActualUI extends JFrame implements ActionListener {
 	/**
 	 * @author Luke Freeman
@@ -17,6 +30,19 @@ public class ActualUI extends JFrame implements ActionListener {
 	public void setUpMenu() {
 		JMenuBar bar = new JMenuBar();
 		JMenu mnuFile = new JMenu("File");
+		JMenuItem miOpenFile = new JMenuItem("Open File");
+		miOpenFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					JFileChooser jfc = new JFileChooser();
+					if (jfc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+						System.out.println(jfc.getSelectedFile().getPath());
+					}
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(null, "Something went wrong");
+				}
+			}
+		});
 		JMenuItem miExit = new JMenuItem("Exit");
 		miExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -49,20 +75,71 @@ public class ActualUI extends JFrame implements ActionListener {
 	 * btnSaveToJson
 	 */
 	public ActualUI() {
+
+		
 		//sets up the look inside this constructor
+		JPanel panSouth = new JPanel();
+		panSouth.setLayout(new FlowLayout());
+		ButtonHandler handlerForText = new ButtonHandler();
+		ButtonHandler handlerForJson = new ButtonHandler();
 		setTitle("Final Project");
-		setBounds(100,200,400,300);
+		setBounds(50,100,400,300);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		JTextArea showText = new JTextArea();
 		Container c = getContentPane();
 		c.setLayout(new BorderLayout());
+		c.add(showText, BorderLayout.CENTER);
 		JButton btnSaveToText = new JButton("Save to text");
+		btnSaveToText.setBackground(Color.BLUE);
 		JButton btnSaveToJson = new JButton("Save to JSON");
+		btnSaveToJson.setBackground(Color.BLUE);
 		JButton btnFile = new JButton("File");
 		JButton btnHelp = new JButton("Help");
-		c.add(btnSaveToText,BorderLayout.SOUTH);
-		c.add(btnSaveToJson,BorderLayout.SOUTH);
+		btnSaveToText.addActionListener(handlerForText);
+		btnSaveToJson.addActionListener(handlerForJson);
+		panSouth.add(btnSaveToText);
+		panSouth.add(btnSaveToJson);
+		btnSaveToText.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnSaveToJson.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
 		btnHelp.addActionListener(this);
 		setUpMenu();
+		
+		JLabel urlLabel = new JLabel("Enter URL");
+		JTextField url1 = new JTextField(20);
+		JButton btnGrabData = new JButton("grab");
+		btnGrabData.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					String urlText = "";
+					String line;
+					URL url = new URL(url1.getText());
+					URLConnection connect = url.openConnection();
+					BufferedReader br = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+					while ((line = br.readLine()) != null) {
+						urlText += line;
+					}
+					String DataToShow = "";
+					DataToShow = DataToShow + urlText;
+					showText.setText(DataToShow);
+				} catch (Exception ex) {
+					System.out.println("Something went wrong when connecting to the URL");
+				}
+			}
+		});
+		JPanel panNorth = new JPanel();
+		panNorth.setLayout(new FlowLayout());
+		panNorth.add(urlLabel);
+		panNorth.add(url1);
+		panNorth.add(btnGrabData);
+		c.add(panNorth, BorderLayout.NORTH);
 	}
 	public static void main (String [] args) {
 		ActualUI AUI = new ActualUI();
